ef7a1c0ec24368ac34a7c9c7f8dcb0963e67b807
Nichprice <97806706+Nichprice@users.noreply.github.com>
2022-09-25 20:26:43 -0400

    number line jumps

---
+++ number_line_jumps.py
@@ -0,0 +1,15 @@
+#add x1 and x2 to arrays [first] and [seceond]
+#for n in range 0 to infinity
+#  
+#for a range of 1 to infinity, multiply n times the v, add to the x, add to the array
+#if the array[n] of first > array[n] of second and array[n+1] of first 
+
+
+def kangaroo(x1, v1, x2, v2):
+  first = [x1]
+  secont = [x2]
+    if x1 == x2:
+      print('YES')
+    else:
+      while
+      first.append(first[])
