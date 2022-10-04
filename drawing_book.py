9bd251d62a0c74219352f57f9ca2f5402476e95b
Nichprice <97806706+Nichprice@users.noreply.github.com>
2022-10-04 11:21:59 -0400

    drawing book solved

---
+++ drawing_book.py
@@ -0,0 +1,37 @@
+n = 6
+p = 5
+
+def pageCount(n, p):
+    # Write your code here
+    turns = []
+    from_start = p - 1
+    if from_start == 0:
+        turns.append(0)
+    elif from_start % 2 == 0:
+        turns.append(from_start / 2)
+    elif from_start % 2 != 0:
+        adjusted = from_start + 1
+        turns.append(adjusted / 2)
+    from_end = n - p
+    if n % 2 == 0:
+        if from_end < 1:
+            turns.append(0)
+        elif from_end == 1:
+            turns.append(1)
+        elif from_end > 1:
+            if from_end % 2 == 0:
+                turns.append(from_end / 2)
+            else:
+                turns.append((from_end - 1) / 2)
+    elif n % 2 != 0:
+        if from_end <= 1:
+            turns.append(0)
+        elif from_end > 1:
+            if from_end % 2 == 0:
+                turns.append(from_end / 2)
+            else:
+                turns.append((from_end - 1) / 2)
+    return int(min(turns))
+
+
+pageCount(n, p)
