29cbd08c6b6767eae19518b1abbcfcefc511217a
Nichprice <97806706+Nichprice@users.noreply.github.com>
2022-10-27 11:56:28 -0400

    encrytion

---
+++ hackerrank/encryption.py
@@ -0,0 +1,13 @@
+def encryption(s):
+    # Write your code here
+    sq_rt = len(s) ** .5
+    col = math.ceil(sq_rt)
+    ans = []
+    for num in range(col):
+        ans.append([])
+    for i in range(len(s)):
+        index = i % col
+        ans[i % col].append(s[i])
+    for num in range(col):
+        ans[num] = (''.join(ans[num]))
+    return ' '.join(ans)
--- hackerrank/kaprekarNumbers.py
+++ hackerrank/kaprekarNumbers.py
@@ -2,15 +2,14 @@ def kaprekarNumbers(p, q):
     # Write your code here
     ans = []
     for num in range(p, q + 1):
-        sq = num ** 2
-        string = str(sq)
+        sq = str(num ** 2)
         d = len(str(num))
-        sq_d = len(string)
+        sq_d = len(sq)
         if num == 1:
             ans.append(num)
         elif num >= 9 and (sq_d == 2 * d or sq_d == (2 * d) - 1):
-            r = int(string[-d:])
-            l = int(string[:(sq_d - d)])
+            r = int(sq[-d:])
+            l = int(sq[:(sq_d - d)])
             if r + l == num:
                 ans.append(num)
                 
