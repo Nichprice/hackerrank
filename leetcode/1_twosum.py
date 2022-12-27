0505614d42594a962c2db57339dc08f38d200903
Nichprice <97806706+Nichprice@users.noreply.github.com>
2022-12-27 15:32:52 -0500

    O(n)

--- leetcode/1_twosum.py
+++ leetcode/1_twosum.py
@@ -1,3 +1,8 @@
+
+
+## O(n^2)
+
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
@@ -5,3 +10,19 @@ class Solution:
             for j in range((i+1), len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i, j]
+
+
+# can you make something quicker than O(n^2)
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        lagmap = {} ## val : index
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in lagmap:
+                return [lagmap[diff], i]
+
+            lagmap[n] = i
+
+        return
