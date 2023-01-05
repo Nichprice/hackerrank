be9a6369376ba16a71193824d65381ceb387e128
Nichprice <97806706+Nichprice@users.noreply.github.com>
2023-01-05 15:45:41 -0500

    2520 build an array from permutation

---
+++ leetcode/2520_count_the_digits_that_divide.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countDigits(self, num: int) -> int:
+
+        ans = 0
+
+        string = str(num)
+
+        for i in range(len(string)):
+            val = int(string[i])
+            if num % val == 0:
+                ans += 1
+
+        return ans
