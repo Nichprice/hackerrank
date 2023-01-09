9d6225cfa82e662db9ceda8c2c461144743a2509
Nichprice <97806706+Nichprice@users.noreply.github.com>
2023-01-09 17:53:44 -0500

    redo of 27

--- leetcode/27_remove_element.py
+++ leetcode/27_remove_element.py
@@ -1,3 +1,30 @@
+## this solution is how you would do it in an interview
+## set a left pointer and a right pointer
+## move the left pointer every time you encounter a unique number
+## always move the right pointer
+## return the left pointer (number of unique numbers)
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        
+        l = 1
+        r = 1
+        val = nums[0]
+
+        while r < len(nums):
+            if nums[r] != nums[l - 1]:
+                nums[l] = nums[r]
+                l += 1
+                r += 1
+            else:
+                r += 1
+
+        return l
+
+
+## this solution is very bad
+
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
 
@@ -18,4 +45,6 @@ class Solution:
                 i += 1
 
         return len(nums)
+
+
 
