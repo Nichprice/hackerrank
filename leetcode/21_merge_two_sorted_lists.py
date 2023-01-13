b23bda11c072ab327504e6eaca47569b7e72495f
Nichprice <97806706+Nichprice@users.noreply.github.com>
2023-01-13 14:14:32 -0500

    merge two sorted linked lits hardcode

--- leetcode/21_merge_two_sorted_lists.py
+++ leetcode/21_merge_two_sorted_lists.py
@@ -1,21 +1,45 @@
+### the wrong way (my first try)
+
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode()
-        tail = dummy
+        one = list1
+        two = list2
+        ans = None
 
-        while list1 and list2:
-            if list1.val < list2.val:
-                tail.next = list1
-                list1 = list1.next
+        head = ans
+        while one or two:
+            if not ans:
+                if not one:
+                    ans = two
+                    two = two.next
+                elif not two:
+                    ans = one
+                    one = one.next
+                elif one.val < two.val or one.val == two.val:
+                    ans = one
+                    one = one.next
+                elif two.val < one.val:
+                    ans = two
+                    two = two.next
+                
+                head = ans
+
             else:
-                tail.next = list2
-                list2 = list2.next
-
-            tail = tail.next
-        
-        if list1:
-            tail.next = list1
-        elif list2:
-            tail.next = list2
-
-        return dummy.next
+                if not one:
+                    ans.next = two
+                    ans = ans.next
+                    two = two.next
+                elif not two:
+                    ans.next = one
+                    ans = ans.next
+                    one = one.next
+                elif one.val < two.val or one.val == two.val:
+                    ans.next = one
+                    ans = ans.next
+                    one = one.next
+                elif two.val < one.val:
+                    ans.next = two
+                    ans = ans.next
+                    two = two.next
+    
+        return head
