34be8a6a2345f7144bb6523a2089e74e762d5051
Nichprice <97806706+Nichprice@users.noreply.github.com>
2023-01-13 13:19:10 -0500

    reverse linked list round 2

--- leetcode/206_reverse_linked_list.py
+++ leetcode/206_reverse_linked_list.py
@@ -1,3 +1,17 @@
+# class Solution:
+#     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+#         prev, curr = None, head
+
+#         while curr:
+#             nxt = curr.next
+#             curr.next = prev
+#             prev = curr
+#             curr = nxt
+        
+#         return prev
+
+## redoing this now that I know what a singly linked list is
+
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         prev, curr = None, head
@@ -7,5 +21,5 @@ class Solution:
             curr.next = prev
             prev = curr
             curr = nxt
-        
+
         return prev
