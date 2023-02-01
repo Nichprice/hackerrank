e8bdbd7c671ee74d2154f7e18c9f79f578868aa5
Nichprice <97806706+Nichprice@users.noreply.github.com>
2023-02-01 06:17:55 -0700

    excel sheet to column title update

--- leetcode/168_excel_sheet_column_title.py
+++ leetcode/168_excel_sheet_column_title.py
@@ -1,15 +1,12 @@
 class Solution:
-    def convertToTitle(self, columnNumber: int) -> str:
-        alpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-
-        mul = columnNumber / 26
-        rem = columnNumber % 26
-
+    def convertToTitle(self, n: int) -> str:
+        
         ans = ''
-        
-        
-        
-        if rem != 0:
-            ans += alpha[rem - 1]
-        
+
+        while n > 0:
+            c = chr(ord('A') + (n - 1) % 26)
+            ans = c + ans
+            n = (n - 1) // 26
+
         return ans
+        
